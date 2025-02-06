ea700afe1c3fab3f6a071b1967dbc12254053db8
Mmonali99 <misromonali1999@gmail.com>
2025-02-06 16:44:10 +0530

    remove backend file sfrom the repository

--- expense-tracker-backend/src/main/java/com/example/expensetrack/exception/BudgetNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.expensetrack.exception;
-
-public class BudgetNotFoundException extends RuntimeException {
-    public BudgetNotFoundException(String message) {
-        super(message);
-    }
-}
--- expense-tracker-backend/src/main/java/com/example/expensetrack/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.expensetrack.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String username) {
-        super("User not found with username: " + username);
-    }
-}
