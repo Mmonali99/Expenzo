a83cb2410d24d9f6e3f03a71c79937f657a3abd5
Mmonali99 <161291906+Mmonali99@users.noreply.github.com>
2025-02-06 21:13:13 +0530

    Delete expense-tracker-backend directory

--- expense-tracker-backend/src/main/java/com/example/expensetrack/exception/CategoryTypeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.expensetrack.exception;
-
-public class CategoryTypeNotFoundException extends RuntimeException {
-    public CategoryTypeNotFoundException(String message) {
-        super(message);
-    }
-}
--- expense-tracker-backend/src/main/java/com/example/expensetrack/repository/CategoryTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.expensetrack.repository;
-
-import com.example.expensetrack.model.CategoryType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryTypeRepository extends JpaRepository<CategoryType, Integer> {
-}
